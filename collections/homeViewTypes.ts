fc441f8d2d45c81263165dea21e938aacd1b45e0
mrsky1001 <nikita.nk16@yandex.ru>
2022-08-12 12:40:30 +0600

    update

---
+++ collections/homeViewTypes.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright (©) 09.07.2021, 17:13. Kolyada Nikita Vladimirovich (nikita.nk16@yandex.ru)
+ */
+
+export default {
+    CARDS: 0,
+    TABLE: 1,
+    DRAFT: 2,
+    PUBLISHED: 3,
+}
