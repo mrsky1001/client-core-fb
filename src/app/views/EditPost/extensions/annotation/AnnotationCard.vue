bb63fd6f3f09bac80990a30456b3eea5b0fddde1
mrsky1001 <nikita.nk16@yandex.ru>
2022-08-22 16:41:18 +0600

    delete and add to core

---
+++ src/app/views/EditPost/extensions/annotation/AnnotationCard.vue
@@ -0,0 +1,94 @@
+<!--
+  - Copyright (©) 09.07.2021, 17:13. Kolyada Nikita Vladimirovich (nikita.nk16@yandex.ru)
+  -->
+
+<template>
+    <v-sheet shaped class="my-sheet">
+        <section-editor />
+        <v-divider></v-divider>
+        <v-card class="my-annotation-card">
+            <v-card-title>Редактирование статьи<span class="red--text">*</span></v-card-title>
+            <v-card-text class="my-card-text">
+                <v-textarea
+                    outlined
+                    auto-grow
+                    rows="1"
+                    label="Заголовок"
+                    class="my-title"
+                    :value="editST.post.title"
+                    @input="(val) => editST.setTitle(val)"
+                    @change="(val) => editST.setTitle(val)"
+                ></v-textarea>
+                <div class="my-annotation">
+                    <v-textarea
+                        auto-grow
+                        rows="5"
+                        label="Аннотация"
+                        outlined
+                        :value="editST.post.annotation.text"
+                        @input="(val) => editST.setAnnotationText(val)"
+                        @change="(val) => editST.setAnnotationText(val)"
+                    ></v-textarea>
+                </div>
+                <tags></tags>
+                <annotation-img></annotation-img>
+            </v-card-text>
+        </v-card>
+    </v-sheet>
+</template>
+
+<script lang="ts">
+import Component from 'vue-class-component'
+import AnnotationImg from '@/app/views/EditPost/extensions/annotation/AnnotationImg.vue'
+import Vue from 'vue'
+import { vxa } from '@/app/store/store.app'
+import SectionEditor from '../sectionEditor/SectionEditor.vue'
+import Tags from '@/app/views/EditPost/extensions/Tags.vue'
+
+@Component({ components: { SectionEditor, Tags, AnnotationImg } })
+export default class AnnotationCard extends Vue {
+    editST = vxa.edit
+}
+</script>
+
+<style lang="scss" scoped>
+.my-annotation-card {
+    //box-shadow: 0 10px 20px 0 rgba(0, 0, 0, 0.1) !important;
+    box-shadow: none !important;
+
+    .v-card__title {
+        color: rgba(0, 0, 0, 0.6);
+        //padding: 16px 0;
+    }
+
+    .my-annotation {
+        //padding: 0 20px;
+
+        font-weight: normal;
+    }
+
+    .my-title {
+        font-size: large;
+        //padding-top: 30px;
+        //padding-left: 20px;
+        //padding-right: 20px;
+    }
+}
+
+@media screen and (max-width: 1000px) {
+    .v-card__text,
+    .my-title,
+    .my-annotation,
+    .v-card__subtitle,
+    .v-card__text,
+    .v-card__title {
+        padding-top: initial;
+        padding-left: 0 !important;
+        //margin: 0 !important;
+    }
+
+    .my-title {
+        font-size: large !important;
+    }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.min.css'
+import '../public/assets/fonts/source sans pro/sourcesanspro.css'
+Vue.config.productionTip = false
+new Vue({
+  router: router,
+  store: store,
+  render: function (h) {
+    return h(App)
+  }
+}).$mount('#app')
+//# sourceMappingURL=main.js.map
